732188add1596008fd01a5407182c5e3ed4bb390
DoanJackson <anhnonono0@gmail.com>
2024-11-04 20:51:02 +0700

    Add websocket get funding rate, funding rate countdown

--- backend/services/price-service/utils/show_error.go
+++ backend/services/price-service/utils/show_error.go
@@ -1,7 +1,19 @@
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
 
 func ShowError(statusCode int64, message string, context *gin.Context) {
 	context.JSON(int(statusCode), gin.H{"message": message})
+}
+
+func ShowErrorSocket(ws *websocket.Conn, message string) {
+	var errorMsg = fmt.Sprintf(`
+	"message": %s
+	`, message)
+	ws.WriteMessage(websocket.TextMessage, []byte(errorMsg))
 }
