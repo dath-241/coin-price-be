297a49c874bd83b0ee22f89cdbc96cfaadabe6ae
HienLe2004 <hien.lengoc2004@hcmut.edu.vn>
2024-11-16 20:55:39 +0700

    Clean main branch

--- backend/services/price-service/utils/show_error.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "github.com/gin-gonic/gin"
-
-func ShowError(statusCode int64, message string, context *gin.Context) {
-	context.JSON(int(statusCode), gin.H{"message": message})
-}
