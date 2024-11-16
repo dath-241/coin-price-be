c794859f7fba0757df27bf48ad6f51b4f9ba4caa
tacsquang <tac.tranquang@hcmut.edu.vn>
2024-11-16 16:51:45 +0700

    update routes.go

--- backend/services/admin_service/src/routes/routes.go
+++ backend/services/admin_service/src/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"backend/services/admin_service/src/middlewares"
 	"backend/services/admin_service/src/controllers"
+	"backend/services/admin_service/src/momo"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,11 +12,15 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	// Route User Management API (Quản lý tài khoản)
-	user := r.Group("/api/v1/user")
+	userRoutes := r.Group("/api/v1/user")
 	{
-		user.GET("/me", middlewares.AuthMiddleware("VIP-0", "VIP-1", "VIP-2", "VIP-3", "Admin"), controllers.GetCurrentUserInfo())          // Lấy thông tin tài khoản người dùng hiện tại.
-		user.PUT("/me", middlewares.AuthMiddleware("VIP-0", "VIP-1", "VIP-2", "VIP-3", "Admin"), controllers.UpdateCurrentUser())           // Chỉnh sửa thông tin tài khoản người dùng.
-		user.DELETE("/me", middlewares.AuthMiddleware("VIP-0", "VIP-1", "VIP-2", "VIP-3", "Admin"), controllers.DeleteCurrentUser())           // Xóa tài khoản người dùng hiện tại.
+		// Áp dụng AuthMiddleware cho tất cả các route trong group này
+		userRoutes.Use(middlewares.AuthMiddleware("VIP-0", "VIP-1", "VIP-2", "VIP-3", "Admin"))
+
+		userRoutes.GET("/me", controllers.GetCurrentUserInfo())          // Lấy thông tin tài khoản người dùng hiện tại.
+		userRoutes.PUT("/me", controllers.UpdateCurrentUser())           // Chỉnh sửa thông tin tài khoản người dùng.
+		userRoutes.DELETE("/me", controllers.DeleteCurrentUser())           // Xóa tài khoản người dùng hiện tại.
+		userRoutes.GET("/me/payment-history", controllers.GetPaymentHistory())
 	}
 
 	// Route cho xác thực
@@ -26,16 +31,20 @@ func SetupRouter() *gin.Engine {
 		authRoutes.POST("/register", controllers.Register())              // Người dùng đăng kí
 		authRoutes.POST("/forgot-password", controllers.ForgotPassword()) // Người dùng quên mật khẩu
 		authRoutes.POST("/reset-password", controllers.ResetPassword())   // Người dùng thay đổi mật khẩu
-		authRoutes.POST("/refresh-token", middlewares.AuthMiddleware("VIP-0", "VIP-1", "VIP-2", "VIP-3", "Admin"), controllers.RefreshToken())     // Người dùng refresh token
+		authRoutes.POST("/refresh-token", controllers.RefreshToken())     // Người dùng refresh token
 		authRoutes.POST("/logout", middlewares.AuthMiddleware("VIP-0", "VIP-1", "VIP-2", "VIP-3", "Admin"), controllers.Logout())                  // Người dùng đăng xuất
 	}
 
 	// Route cho quản trị viên (admin)
 	adminRoutes := r.Group("/api/v1/admin/")
 	{
-		adminRoutes.GET("/users", middlewares.AuthMiddleware("Admin"), controllers.GetAllUsers())            //Lay thong tin tat ca nguoi dung
-		//adminRoutes.GET("/user/:user_id", middlewares.AuthMiddleware("Admin"), controllers.GetUserByAdmin()) //Lay thong tin 1 user cu the
-		//adminRoutes.DELETE("/user/:user_id", middlewares.AuthMiddleware("Admin"), controllers.DeleteUser())  //Xóa 1 người dùng
+		// Áp dụng AuthMiddleware cho tất cả các route trong group này
+		adminRoutes.Use(middlewares.AuthMiddleware("Admin"))
+
+		adminRoutes.GET("/users", controllers.GetAllUsers())            //Lay thong tin tat ca nguoi dung
+		adminRoutes.GET("/user/:user_id", controllers.GetUserByAdmin()) //Lay thong tin 1 user cu the
+		adminRoutes.DELETE("/user/:user_id", controllers.DeleteUserByAdmin())  //Xóa 1 người dùng
+		adminRoutes.GET("/payment-history", controllers.GetPaymentHistoryForAdmin())
 	}
 
 	// Route cho payment 
@@ -43,6 +52,8 @@ func SetupRouter() *gin.Engine {
 	{
 		paymentRoutes.POST("/vip-upgrade", middlewares.AuthMiddleware("VIP-0", "VIP-1", "VIP-2"), controllers.CreateVIPPayment())
 		paymentRoutes.POST("/confirm", controllers.ConfirmPaymentHandlerSuccess())
+		paymentRoutes.POST("/status", controllers.HandleQueryPaymentStatus())
+		paymentRoutes.POST("/momo-callback", momo.MoMoCallback())
 	}
 
 	return r
