1e4f23b0c1992a69c7ae8fd502a8b947279c6be3
quachkhaihao <hao.quachqkh9104cs@hcmut.edu.vn>
2024-11-24 20:39:24 +0700

    delete user.go model in trigger-service

--- backend/services/trigger-service/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// User đại diện cho một người dùng trong hệ thống
-type User struct {
-	ID     string  `json:"id"`     // ID của người dùng
-	Email  string  `json:"email"`  // Email của người dùng
-	Alerts []Alert `json:"alerts"` // Danh sách các cảnh báo của người dùng
-}
