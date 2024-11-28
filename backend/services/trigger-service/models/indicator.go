f8ffe703cb96a9466570f05ba47fbfd9940ad949
HienLe2004 <hien.lengoc2004@hcmut.edu.vn>
2024-11-28 22:45:02 +0700

    Update CI production

--- backend/services/trigger-service/models/indicator.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Indicator struct {
-	ID                 string `bson:"_id,omitempty" json:"id"`
-	Symbol             string `json:"symbol" binding:"required"`
-	Indicator          string `json:"indicator" binding:"required"`
-	Period             int    `json:"period" binding:"required"`
-	NotificationMethod string `json:"notification_method" binding:"required"`
-}
